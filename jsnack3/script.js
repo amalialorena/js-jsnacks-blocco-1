542b4578ed56c5928f46ea992371b38446f2112c
Amalia <amalialorenaferariu@gmail.com>
2021-10-28 15:09:40 +0200

    jsnack3 parziale

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,7 +1,24 @@
 // 3 - Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 
-// Creo un prompt che chiede 10 volte all’utente di inserire un numero.
+// Creo un prompt che chiede all’utente di inserire un numero.
+// let numbers = parseInt(prompt("Inserisci un numero"));
+
+// Lo ricreo 10 volte con un ciclo     
     //salvo i numeri dentro a un array 
 
+let numbersArray = []
+while (numbersArray.length < 3) {
+    let numbers = parseInt(prompt("Inserisci un numero"));
+    numbersArray.push(numbers);
+}
+console.log(numbersArray)
+
     //faccio un ciclo che mi somma tutti i numeri
+
+// let i = 0;
+// while (i < numbersArray.length) {
+//     let sum = numbersArray[i]   
+//     i++
+
+// }  
