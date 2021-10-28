6405d0ab7daf75ea0a4392d38e2cbd1d4fc8f526
Amalia <amalialorenaferariu@gmail.com>
2021-10-28 13:57:01 +0200

    jsnack 1 logica

---
+++ jsnack1/script.js
@@ -0,0 +1,11 @@
+// 1 - L’utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore.
+
+
+// creare il primo prompt
+    //salvare il suo valore in una variabile
+//creare il secondo prompt
+    //salvare il suo valore in una variabile
+
+//fare la comparazione tra i due valori 
+    //stampare il numero più grande nella console
