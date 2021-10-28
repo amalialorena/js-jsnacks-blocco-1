fd0497fa9841b3a2dce1527213a0a2090fcbb376
Amalia <amalialorenaferariu@gmail.com>
2021-10-28 14:32:40 +0200

    jsnack logica

---
+++ jsnack3/script.js
@@ -0,0 +1,7 @@
+// 3 - Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+// Creo un prompt che chiede 10 volte all’utente di inserire un numero.
+    //salvo i numeri dentro a un array 
+
+    //faccio un ciclo che mi somma tutti i numeri
