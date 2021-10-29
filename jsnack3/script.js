af600d498609b68bf1a9f1d90a707b985f800dab
Amalia <amalialorenaferariu@gmail.com>
2021-10-29 10:20:46 +0200

    jsnack 5 indentazione

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -2,26 +2,25 @@
 // Il programma stampa la somma di tutti i numeri inseriti.
 
 // Creo un prompt che chiede all’utente di inserire un numero.
-// let numbers = parseInt(prompt("Inserisci un numero"));
-
 // Lo ricreo 10 volte con un ciclo     
     //salvo i numeri dentro a un array 
 
-    // Ci sono quasi
-let numbersArray = []
-while (numbersArray.length < 3) {
+let numbersArray = [];
+let sum = 0;
+for (let i = 0; i < 10; i++) {
     let numbers = parseInt(prompt("Inserisci un numero"));
     numbersArray.push(numbers);
+    sum += numbersArray[i];
 }
-console.log(numbersArray)
+console.log("il mio array:", numbersArray)
 
-    //faccio un ciclo che mi somma tutti i numeri
+// oppure:
 
-let number = numbersArray [0];
-let sum = 0;
-console.log("number è: " ,number)
-for (let i = 0; i < numbersArray.length; i++) {
-    sum = number + numbersArray [i];
-}
+// let sum = 0;
+// for (let i = 0; i < 10; i++) {
+//     let numbers = parseInt(prompt("Inserisci un numero"));
+//     sum += numbers;
+// }
+
 
 console.log("la somma è:" ,sum);
--- jsnack5/script.js
+++ jsnack5/script.js
@@ -6,8 +6,8 @@ for (let i = 0; i < 6; i++) {
     // Chiedi per 6 volte all’utente di inserire un numero,
     let num = parseInt(prompt("inserisci un numero"));
     // se è dispari inseriscilo nell’array. 
-        if (num % 2 !== 0) {
-            numbers.push(num);
-        } 
+    if (num % 2 !== 0) {
+         numbers.push(num);
+    } 
 }
 console.log(numbers);
